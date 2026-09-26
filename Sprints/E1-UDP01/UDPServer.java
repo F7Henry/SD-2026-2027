f4b7ca6678b2a643180ee695751e1a5d8302d8e5
F7Henry <henriquefelisberto2007@gmail.com>
2026-09-26 02:01:10 +0100

    tarefa 2 hashmap
    criação do hashmap e da função processDeliveredMessages, no UDPServer.java

--- Sprints/E1-UDP01/UDPServer.java
+++ Sprints/E1-UDP01/UDPServer.java
@@ -1,7 +1,34 @@
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
 
 public class UDPServer {
+
+    public static HashMap<Integer, String> mapaRecebidos = new HashMap<>();
+
+    public static int processDeliveredMessages(
+            int nLastMessageInOrder,
+            int nCurrentMessage,
+            String currentMessage
+    ) {
+        //coisas
+        if (nCurrentMessage == nLastMessageInOrder + 1) {
+            //coisas
+            System.out.println("Mensagem entregue: " + currentMessage);
+            nLastMessageInOrder++;
+        }
+        else if (nCurrentMessage > nLastMessageInOrder + 1) {
+            //coisas
+            System.out.println("Fora de ordem: " + currentMessage);
+            System.out.println("  > Aguardando mensagem de número: " + (nLastMessageInOrder + 1));
+            mapaRecebidos.putIfAbsent(nCurrentMessage, currentMessage);
+        }
+        else {
+            //coisas
+            System.out.println("Mensagem já recebida!");
+        }
+        return nLastMessageInOrder;
+    }
 
     public static void main(String args[]) {
         DatagramSocket aSocket = null;
@@ -57,23 +84,28 @@ public class UDPServer {
 
 
 
-
-                if (numero == L + 1) {
-
-                    reply = new DatagramPacket(request.getData(),
-                            request.getLength(), request.getAddress(), request.getPort());
-                    L = numero;
+                int novoL = processDeliveredMessages(L, numero, mensagem);
+                if (novoL != L) {
+                    L = novoL;
+                    while (mapaRecebidos.containsKey(L + 1)) {
+                        int proximoNumero = L + 1;
+                        String proximaMensagem = mapaRecebidos.remove(proximoNumero);
+                        L = processDeliveredMessages(L, proximoNumero, proximaMensagem);
+                    }
+                    resposta = "delivered," + L;
                 }
                 else {
                     resposta = "waitingfor," + (L + 1);
-                    byte[] dadosResposta = resposta.getBytes();
-                    reply = new DatagramPacket(
-                            dadosResposta,
-                            dadosResposta.length,
-                            request.getAddress(),
-                            request.getPort()
-                    );
                 }
+
+                byte[] dadosResposta = resposta.getBytes();
+                reply = new DatagramPacket(
+                        dadosResposta,
+                        dadosResposta.length,
+                        request.getAddress(),
+                        request.getPort()
+                );
+
                 aSocket.send(reply);
             }
         } catch (SocketException e) { System.out.println("Socket: " + e.getMessage());
